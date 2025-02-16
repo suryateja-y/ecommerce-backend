d5a4e1db207cb79b958cea68034f777dc292835e
Surya Teja <yeniberasuryateja@gmail.com>
2025-02-16 18:25:47 +0530

    Added Distributed Tracing using Zipkin

--- services/cart-management-service/src/main/java/com/academy/projects/ecommerce/cartmanagementservice/clients/configurations/FeignInterceptor.java
+++ services/cart-management-service/src/main/java/com/academy/projects/ecommerce/cartmanagementservice/clients/configurations/FeignInterceptor.java
@@ -1,6 +1,6 @@
 package com.academy.projects.ecommerce.cartmanagementservice.clients.configurations;
 
-import com.academy.projects.ecommerce.cartmanagementservice.security.TokenManager;
+import com.academy.projects.ecommerce.cartmanagementservice.security.TokenStorage;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 
@@ -9,6 +9,6 @@ public class FeignInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
         requestTemplate
-                .header("Authorization", "Gateway " + TokenManager.getToken());
+                .header("Authorization", "Gateway " + TokenStorage.getToken());
     }
 }
